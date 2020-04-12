3ccf70fb57c3d1eaa517b1d72750d9c292b8fc60
ziobroa <ziobroa@BY1928A75002192.wit.private>
2020-04-12 16:22:10 -0400

    First Commit, Basic Window.

---
+++ TicTacToe/src/application/MainController.java
@@ -0,0 +1,43 @@
+package application;
+	
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+//I legit have no idea what is happening right now.
+public class MainController extends Application implements Initializable {
+	@FXML
+	Button tic1;
+	@Override
+	public void start(Stage TicTacToe) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("TicTacToe.fxml"));
+	        Scene scene = new Scene(root);
+	        TicTacToe.setScene(scene);
+	        TicTacToe.setTitle("Tic Tac Toe");
+	        TicTacToe.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+}
