f807b80b9c40135903d68a22c2270697b785009d
ziobroa <ziobroa@BY1928A75002192.wit.private>
2020-04-16 18:45:27 -0400

    This class is the basis of the gameTiles on the board.
    We can declare these with their specific buttons such that we can make
    it so that the buttons are not edited after their creation. I think.

---
+++ TicTacToe/src/application/GameTile.java
@@ -0,0 +1,47 @@
+package application;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GameTile 
+{
+	@FXML
+	protected Button gameButton;
+	protected int ownerID;
+	protected boolean isLocked;
+	
+	public GameTile(Button s)
+	{
+		this.ownerID=-1;
+		this.gameButton=s;
+		this.isLocked=false;
+	}
+	public void changeOID(int oid)
+	{
+		this.ownerID=oid;
+		lock();
+	}
+	public void lock()
+	{
+		this.isLocked=true;
+	}
+	public void unlock()
+	{
+		this.isLocked=false;
+	}
+	
+}
