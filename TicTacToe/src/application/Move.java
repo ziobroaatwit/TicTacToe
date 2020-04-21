7517f6b34c174aa5b119503ff79bd0f5114f1396
ziobroa <ziobroa@BY1928A75002192.wit.private>
2020-04-21 17:14:49 -0400

    JavaDoc documentation added.

--- TicTacToe/src/application/Move.java
+++ TicTacToe/src/application/Move.java
@@ -1,22 +1,45 @@
 package application;
-
+/**
+ * This class contains the Move object constructors, and its methods
+ * @author ziobroa
+ *
+ */
 public class Move 
 {
 	protected int index;
 	protected int score;
+	/**
+	 * This original constructor would have been used in the 
+	 * context of a minimax algorithm but I could not get it functioning
+	 * in time.
+	 * @param i
+	 * @param s
+	 */
 	public Move(int i,int s)
 	{
 		this.index=i;
 		this.score=s;
 	}
+	/**
+	 * This scoreless constructor simply holds the index of the move.
+	 * @param i
+	 */
 	public Move(int i)
 	{
 		this.index=i;
 	}
+	/**
+	 * If a score exists, it returns it.
+	 * @return
+	 */
 	public int getScore()
 	{
 		return this.score;
 	}
+	/**
+	 * Returns the index.
+	 * @return
+	 */
 	public int getIndex()
 	{
 		return this.index;
